629721e9dcb4a4aaabc1d713697109029c72aa34
Carlo Capocasa <carlo@carlocapocasa.com>
2014-04-01 22:00:50 +0200

    working vars

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 
-
 # Sane Wrangler
 
 # An attempt to make SANE less impossible to install on OSX
@@ -8,25 +7,45 @@
 
 require 'vendor/autoload.php';
 
-# Detect OSX version
+$linesFile = __DIR__.'/cache/osxsane/versions';
 
-$bc = new phpbrowscap\Browscap('cache/browscap');
-
-$b = $bc->getBrowser();
-
-$err = array();
-
-$version = isset($_GET['version']) ? $_GET['version']  : $b->Platform_Version;
-
+# Populate parameters
+$version = isset($_GET['version']) ? $_GET['version']  : null;
+$tool = $_GET['tool'];
+# Validate parameters
 if (null == preg_match('#10\\.[2-9]#', $version)) {
   $version = null;
-  $err[] = 'Use on OSX or enter version <form style="display:inline" method="get" action=""><input name="version" value="10.6"><input type="submit" value="Go"></form>';
+}
+$tools = array('TWAIN-SANE-Interface', 'SANE-Preference-Pane', 'sane-backends', 'libusb');
+if (false == in_array($tool, $tools)) {
+  $tool = null;
 }
 
-if (count($err) > 0) {
-  echo '<ul style="list-style-type:none;">'.implode('', array_map(function ($err){return "<li>$err</li>";}, $err)).'</ul>';
+# Autodetect
+if ($version == null) {
+  $bc = new phpbrowscap\Browscap('cache/browscap');
+  $b = $bc->getBrowser();
+  if ($b->Platform == 'MacOSC') {
+    $version = $b->Platform_Version;
+  }
+}
+
+# Error messages
+$errs = array();
+if (null == $tool) {  
+  $errs[] = 'Chose tool: '. implode(' ', array_map(function ($tool) use ($version) {return "<a href='?tool=$tool&version=$version'>$tool</a>";}, $tools));
+}
+if (null == $version) {  
+  $errs[] = 'Use on OSX or enter version <form style="display:inline" method="get" action=""><input name="tool" value="'.$tool.'" type="hidden"><input name="version" value="10.6"><input type="submit" value="Go"></form>';
+}
+
+# Compile errors
+
+if (count($errs) > 0) {
+  echo '<ul style="list-style-type:none;">'.implode('', array_map(function ($err){return "<li>$err</li>";}, $errs)).'</ul>';
   exit();
 }
 
+$lines = file($linesFile, FILE_IGNORE_NEW_LINES);
 
 
